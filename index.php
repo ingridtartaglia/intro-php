eff791121dbbd1309e679de5505835cdb3a3ffc9
Ingrid Tartaglia <ingrid.tartaglia@gmail.com>
2016-06-07 23:41:51 -0300

    Variáveis e constantes

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            // Variáveis
+            $ola = 'hello ';
+            $mundo = 'world';
+            echo '<p>' . $ola . $mundo . '</p>';
+
+            // Constantes
+            define('NOME', 'Ingrid');
+            echo NOME;
+
+
+        ?>
+    </body>
+</html>
